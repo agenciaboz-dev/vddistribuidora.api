76849ba84aebd1eb1851afde1b279804ea93bd6c
ispadilha <israel.padilha@gmail.com>
2024-03-01 13:38:57 -0300

    removed git cache

--- dist/src/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.setProd = exports.env = void 0;
-exports.env = "dev";
-const setProd = () => {
-    exports.env = "prod";
-};
-exports.setProd = setProd;
--- dist/src/prisma/include.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.person = void 0;
-const client_1 = require("@prisma/client");
-exports.person = client_1.Prisma.validator()({
-    addresses: true,
-    judiciaryPerson: true,
-    physicalPerson: true,
-});
