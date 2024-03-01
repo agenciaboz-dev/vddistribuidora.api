17ae2820ab7e81cd50c949b616ab73f68f1fded4
nandobfer <nandobfer@yahoo.com.br>
2024-03-01 13:52:54 -0300

    removed cached dist

--- dist/src/prisma/include.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.entity = void 0;
-const client_1 = require("@prisma/client");
-exports.entity = client_1.Prisma.validator()({
-    addresses: true,
-    judiciaryEntity: true,
-    physicalEntity: true,
-});
